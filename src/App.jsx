aa401b94b3a6305954d3c095f3b4442ee0dd91ab
MSarasti <misarasti@hotmail.com>
2023-04-11 18:59:04 -0500

    Updated App to link with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Link } from 'react-router-dom'
 import routes from './config/routes'
 
 function App() {
@@ -7,6 +8,28 @@ function App() {
 
   return (
     <div className="App">
+      <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/people">People</Link>
+                </li>
+                <li>
+                  <Link to="/ship">Starship</Link>
+                </li>
+                <li>
+                  <Link to="/planet">Planet</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Sign In</Link>
+                </li>
+              </ul>
+            </nav>
       {routes}
     </div>
   )

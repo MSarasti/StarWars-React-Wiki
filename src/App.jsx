cca81920661b110265d66b44750f5038f50e302f
MSarasti <misarasti@hotmail.com>
2023-04-11 19:23:10 -0500

    Testing routes for GH-Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,34 @@ import { Link } from 'react-router-dom'
 import routes from './config/routes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/people">People</Link>
-                </li>
-                <li>
-                  <Link to="/ship">Starship</Link>
-                </li>
-                <li>
-                  <Link to="/planet">Planet</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/register">Sign In</Link>
-                </li>
-              </ul>
-            </nav>
-      {routes}
-    </div>
-  )
+	return (
+		<div className="App">
+		<nav>
+			<ul id="navigation">
+				<li>
+				<Link to="/">Home</Link>
+				</li>
+				<li>
+				<Link to="/people">People</Link>
+				</li>
+				<li>
+				<Link to="/ship">Starship</Link>
+				</li>
+				<li>
+				<Link to="/planet">Planet</Link>
+				</li>
+				<li>
+				<Link to="/login">Login</Link>
+				</li>
+				<li>
+				<Link to="/register">Sign In</Link>
+				</li>
+			</ul>
+		</nav>
+		{routes}
+		</div>
+	)
 }
 
 export default App
